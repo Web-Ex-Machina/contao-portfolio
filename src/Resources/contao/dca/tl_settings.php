f9e5ae918f3d4fde185029cd28d8f0c6d3782a51
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-07-31 13:32:20 +0200

    Update encryption methods in `tl_settings` field definition

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -6,22 +6,16 @@ use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 PaletteManipulator::create()
     ->addLegend('portfolio_legend', 'global_legend', PaletteManipulator::POSITION_AFTER)
-//    ->addField('portfolioRemoteWebsite', 'portfolio_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('portfolioApiKey', 'portfolio_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('default', 'tl_settings');
 
-//$GLOBALS['TL_DCA']['tl_settings']['fields']['portfolioRemoteWebsite'] = [
-//    'label' => &$GLOBALS['TL_LANG']['tl_settings']['portfolioApiKey'],
-//    'inputType' => 'text',
-//    'eval' => ['rgxp' => 'url'],
-//];
 $GLOBALS['TL_DCA']['tl_settings']['fields']['portfolioApiKey'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['portfolioApiKey'],
     'inputType' => 'text',
     'load_callback' => [
-        [WEM\UtilsBundle\Classes\Encryption::class, 'decrypt'],
+        [WEM\UtilsBundle\Classes\Encryption::class, 'decrypt_b64'],
     ],
     'save_callback' => [
-        [WEM\UtilsBundle\Classes\Encryption::class, 'encrypt'],
+        [WEM\UtilsBundle\Classes\Encryption::class, 'encrypt_b64'],
     ],
 ];
