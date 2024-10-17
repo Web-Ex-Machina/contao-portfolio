747e138491d7afa4aa82596be5b6d7561bc1a1b6
LupusVII <jln.thirion@gmail.com>
2024-10-17 17:25:03 +0200

    Clean tl_settings

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -5,17 +5,16 @@ declare(strict_types=1);
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 PaletteManipulator::create()
-    ->addLegend('portfolio_legend', 'global_legend', PaletteManipulator::POSITION_AFTER)
-    ->addField('portfolioApiKey', 'portfolio_legend', PaletteManipulator::POSITION_APPEND)
+    ->addLegend('portfolio_legend')
+    ->addField('portfolioApiKey', 'portfolio_legend')
     ->applyToPalette('default', 'tl_settings');
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['portfolioApiKey'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_settings']['portfolioApiKey'],
     'inputType' => 'text',
     'load_callback' => [
-        [WEM\UtilsBundle\Classes\Encryption::class, 'decrypt_b64'],
+        ['wem.encryption_util', 'decrypt_b64'],
     ],
     'save_callback' => [
-        [WEM\UtilsBundle\Classes\Encryption::class, 'encrypt_b64'],
+        ['wem.encryption_util', 'encrypt_b64'],
     ],
 ];
