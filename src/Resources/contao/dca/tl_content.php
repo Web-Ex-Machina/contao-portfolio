79968d17092791d928baa108c5152266cb338afe
LupusVII <jln.thirion@gmail.com>
2024-10-28 10:59:50 +0100

    Move palette manipulator in a onload callback so we can apply the update on all bundles

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -12,14 +12,15 @@ declare(strict_types=1);
  * @link     https://github.com/Web-Ex-Machina/contao-portfolio/
  */
 
-// Dynamically add the permission check and parent table
-use Contao\CoreBundle\DataContainer\PaletteManipulator;
 use Contao\Input;
 use Contao\System;
+use WEM\PortfolioBundle\DataContainer\ContentContainer;
 
 if ('wem_portfolio_feed' === Input::get('do')) {
     $GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_wem_portfolio';
 }
+
+$GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'][] = [ContentContainer::class, 'updatePalettes'];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['wem_language'] = [
     'exclude' => true,
@@ -30,20 +31,3 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['wem_language'] = [
     'eval' => ['includeBlankOption' => true, 'mandatory' => true, 'chosen' => true, 'tl_class' => 'w50'],
     'sql' => "varchar(64) NOT NULL default ''",
 ];
-
-
-if ('wem_portfolio_feed' === Input::get('do')) {
-    foreach ($GLOBALS['TL_DCA']['tl_content']['palettes'] as $key => $value) {
-        if ($key == "__selector__" || $key == "default" ) {continue;}
-
-        PaletteManipulator::create()
-            // apply the field "custom_field" after the field "username"
-            ->addLegend("language")
-            ->addField('wem_language', 'language')
-
-            // now the field is registered in the PaletteManipulator
-            // but it still has to be registered in the globals array:
-            ->applyToPalette($key, 'tl_content')
-        ;
-    }
-}
