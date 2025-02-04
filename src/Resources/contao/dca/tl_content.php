d03c21b4a7279ebe649f0e10b9949a06fc71c87f
LupusVII <jln.thirion@gmail.com>
2025-02-04 19:54:33 +0100

    Extends tl_content to add the portfolio content language within the backend content preview

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -18,10 +18,11 @@ use WEM\PortfolioBundle\DataContainer\ContentContainer;
 
 if ('wem_portfolio_feed' === Input::get('do')) {
     $GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_wem_portfolio';
+
+    $GLOBALS['TL_DCA']['tl_content']['list']['sorting']['child_record_callback'] = ['tl_content_wemportfolio', 'addCteType'];
 }
 
 $GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'][] = [ContentContainer::class, 'updatePalettes'];
-
 $GLOBALS['TL_DCA']['tl_content']['fields']['wem_language'] = [
     'exclude' => true,
     'filter' => true,
@@ -31,3 +32,103 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['wem_language'] = [
     'eval' => ['includeBlankOption' => true, 'mandatory' => true, 'chosen' => true, 'tl_class' => 'w50'],
     'sql' => "varchar(64) NOT NULL default ''",
 ];
+
+/**
+ * Provide miscellaneous methods that are used by the data configuration array.
+ */
+class tl_content_wemportfolio extends tl_content
+{
+    /**
+     * Import the back end user object
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Add the type of content element
+     *
+     * @param array $arrRow
+     *
+     * @return string
+     */
+    public function addCteType($arrRow)
+    {
+        $key = $arrRow['invisible'] ? 'unpublished' : 'published';
+        $type = $GLOBALS['TL_LANG']['CTE'][$arrRow['type']][0] ?? $arrRow['type'];
+        $class = 'limit_height';
+
+        // Remove the class if it is a wrapper element
+        if (in_array($arrRow['type'], $GLOBALS['TL_WRAPPERS']['start']) || in_array($arrRow['type'], $GLOBALS['TL_WRAPPERS']['separator']) || in_array($arrRow['type'], $GLOBALS['TL_WRAPPERS']['stop']))
+        {
+            $class = '';
+
+            if (($group = $this->getContentElementGroup($arrRow['type'])) !== null)
+            {
+                $type = ($GLOBALS['TL_LANG']['CTE'][$group] ?? $group) . ' (' . $type . ')';
+            }
+        }
+
+        // Add the group name if it is a single element (see #5814)
+        elseif (in_array($arrRow['type'], $GLOBALS['TL_WRAPPERS']['single']))
+        {
+            if (($group = $this->getContentElementGroup($arrRow['type'])) !== null)
+            {
+                $type = ($GLOBALS['TL_LANG']['CTE'][$group] ?? $group) . ' (' . $type . ')';
+            }
+        }
+
+        // Add the ID of the aliased element
+        if ($arrRow['type'] == 'alias')
+        {
+            $type .= ' ID ' . $arrRow['cteAlias'];
+        }
+
+        // Add the protection status
+        if ($arrRow['protected'] ?? null)
+        {
+            $groupIds = StringUtil::deserialize($arrRow['groups'], true);
+            $groupNames = array();
+
+            if (!empty($groupIds))
+            {
+                if (in_array(-1, array_map('intval', $groupIds), true))
+                {
+                    $groupNames[] = $GLOBALS['TL_LANG']['MSC']['guests'];
+                }
+
+                if (null !== ($groups = MemberGroupModel::findMultipleByIds($groupIds)))
+                {
+                    $groupNames += $groups->fetchEach('name');
+                }
+            }
+
+            $type .= ' (' . $GLOBALS['TL_LANG']['MSC']['protected'] . ($groupNames ? ': ' . implode(', ', $groupNames) : '') . ')';
+        }
+
+        // Add the headline level (see #5858)
+        if ($arrRow['type'] == 'headline' && is_array($headline = StringUtil::deserialize($arrRow['headline'])))
+        {
+            $type .= ' (' . $headline['unit'] . ')';
+        }
+
+        // Limit the element's height
+        if (!Config::get('doNotCollapse'))
+        {
+            $class .= ' h40';
+        }
+
+        $objModel = new ContentModel();
+        $objModel->setRow($arrRow);
+
+        $arrLanguages = System::getContainer()->get('contao.intl.locales')->getLocales(null, false);
+        $strLang = $arrRow['wem_language'] ? $arrLanguages[$arrRow['wem_language']] : 'NR';
+
+        return '
+<div class="cte_type ' . $key . '">' . $type . ' // ' . $strLang . '</div>
+<div class="' . trim($class) . '">
+' . StringUtil::insertTagToSrc($this->getContentElement($objModel)) . '
+</div>' . "\n";
+    }
+}
