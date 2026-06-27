920bca1354e11c21c7262642aacafa3a2ea08733
LupusVII <jln.thirion@ik.me>
2026-06-27 10:29:19 +0200

    Fix Route loader

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -21,6 +21,7 @@ use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\RouteCollection;
 use WEM\PortfolioBundle\WEMPortfolioBundle;
 
 class Plugin implements BundlePluginInterface, RoutingPluginInterface
@@ -37,7 +38,7 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
         ];
     }
 
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel): RouteCollection|null
     {
         return $resolver
             ->resolve(__DIR__.'/../../config/routes.yaml')
